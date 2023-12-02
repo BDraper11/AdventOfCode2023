b661b281239e49bff01a085452f875d887c60324
BDraper11 <54155379+BDraper11@users.noreply.github.com>
2023-12-02 21:01:04 +0000

    Day 2 Puzz 2 working Nice bit about Class Properties

--- Day2.py
+++ Day2.py
@@ -3,9 +3,10 @@ import re
 
 class Game:
     def __init__(self,Game):
-        self.ID = int()
-        self.Sets = list()
-        self.NumSets = int()
+        self._ID = int()
+        self._Sets = list()
+        self._NumSets = int()
+        self._Power = int()
 
         def ParseGame(Game):
             def ParseID(Game):
@@ -24,19 +25,55 @@ class Game:
                 for Set in tempSets:
                     Sets.append(ParseSet(Set))
                 return Sets
-            self.ID = ParseID(Game)
-            self.Sets = ParseSets(Game)
-            self.NumSets = len(self.Sets)
+            self._ID = ParseID(Game)
+            self._Sets = ParseSets(Game)
+            self._NumSets = len(self._Sets)
             return self
+        
         thisGame = ParseGame(Game)
         return None
+    
+    @property
+    def Sets(self): return self._Sets
+    @Sets.setter
+    def Sets(self, value): self._Sets = value
+    @Sets.deleter
+    def Sets(self): del self._Sets
+
+    @property
+    def ID(self): return self._ID
+    @ID.setter
+    def ID(self, value): self._ID = value
+    @ID.deleter
+    def ID(self): del self._ID
+    
+    @property
+    def Power(self): return self._Power
+    @Power.setter
+    def Power(self, value): self._Power = value
+    @Power.deleter
+    def Power(self): del self._Power
+
+    def CalcPower(self):
+        maxReds = int()
+        maxGreens = int()
+        maxBlues = int()
+
+        for Set in self.Sets:
+            if "red" in Set:
+                maxReds = max(maxReds,int(Set["red"]))
+            if "green" in Set:
+                maxGreens = max(maxGreens,int(Set["green"]))
+            if "blue" in Set:
+                maxBlues = max(maxBlues,int(Set["blue"]))
+        self._Power = (maxReds*maxGreens*maxBlues)
+        return self._Power
 def GetGames(input):
     Games = list()
     for line in input:
         thisGame = Game(line)
         Games.append(thisGame)
     return Games
-
 def CheckSet(Set,REDLIMIT,GREENLIMIT,BLUELIMIT):
     if "red" in Set:
         c1 = int(Set["red"])<=REDLIMIT
@@ -56,23 +93,19 @@ def Puzz1(Games):
     GREENLIMIT = 13
     BLUELIMIT = 14
     
-    reds = list()
-    greens = list()
-    blues = list()
     temp = [aGame for aGame in Games if not [aSet for aSet in aGame.Sets if not CheckSet(aSet,REDLIMIT,GREENLIMIT,BLUELIMIT)]]
     output = sum([x.ID for x in temp])
     print("breakpoint")
     return output
-
 def Puzz2(Games):
-    return 0
-
+    output = [Game.CalcPower() for Game in Games]
+    output = sum(output)
+    return output
 if __name__ == "__main__":
     input = GetInput(2,False)
-    Execute = 1
-
     Games = GetGames(input)
 
+    Execute = 2
     if (Execute & 0b01):
         Puzz1Ans = Puzz1(Games)
         print(Puzz1Ans), pyperclip.copy(Puzz1Ans)
